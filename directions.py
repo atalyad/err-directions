ed407a641717674ff4864c2abcbaaf1a87450cdb
Guillaume BINET <gbin@gootz.net>
2012-07-10 18:05:07 +0200

    changed the format of the directions and generated commands

--- directions.py
+++ directions.py
@@ -1,3 +1,4 @@
+import logging
 import re
 from errbot.botplugin import BotPlugin
 from errbot.jabberbot import botcmd
@@ -7,14 +8,31 @@ from urllib2 import urlopen
 
 __author__ = 'atalyad'
 
-DIRECTIONS_URL = 'http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&sensor=false'
+DIRECTIONS_URL = 'http://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&sensor=false&mode=%s'
+
+
+def generate(mode):
+    f = lambda self, mess, args: self.bare_directions(args, mode)
+    f.__name__ = 'directions_%s' % mode
+    f.__doc__ = "Returns the %s directions from origin to destination using Google directions api.\n!directions_%s origin -> destination" % (mode, mode)
+    return botcmd(f, split_args_with='->')
+
+
+class DirectionsBuilder(type):
+    MODES = ('driving', 'walking', 'bicycling', 'transit')
+    def __new__(mcs, name, bases, classDict):
+        newClassDict = dict(classDict.items() +
+                            [('directions_%s' % mode, botcmd(generate(mode)))
+                            for mode in mcs.MODES])
+        return super(DirectionsBuilder, mcs).__new__(mcs, name, bases, newClassDict)
+
 
 class Directions(BotPlugin):
+    __metaclass__ = DirectionsBuilder
 
     min_err_version = '1.2.1' # make err split the arguments for you
 
     def generate_directions_str(self, json_res):
-
         remove_html_tags = re.compile(r'<.*?>')
         too_many_spaces = re.compile(r'  +')
 
@@ -37,20 +55,9 @@ class Directions(BotPlugin):
                 ans += '\n\n'
         return ans.strip('\n')
 
-
-    @botcmd(split_args_with=':')
-    def directions(self, mess, args):
-        """
-        Returns the directions from origin to destination using Google directions api.
-        !directions origin:destination:mode
-        *mode is not required, defaults to driving.
-        """
-        if len(args) < 2:
-            return 'Please supply an origin and a destination separated by a ":". Travel mode is optional (defaults to driving. Options are: "driving", "walking", "bicycling", "transit").'
-
-        url = DIRECTIONS_URL % (quote(args[0].encode('utf-8')), quote(args[1].encode('utf-8')))
-        if len(args) == 3:
-            url += '&mode=%s' % args[2].strip()
+    def get_directions(self, A, B, mode):
+        logging.debug('Directions from %s to %s by %s' % (A, B, mode))
+        url = DIRECTIONS_URL % (quote(A.encode('utf-8')), quote(B.encode('utf-8')), mode)
 
         content = urlopen(url)
         results = simplejson.load(content)
@@ -59,3 +66,20 @@ class Directions(BotPlugin):
             return results['status']
 
         return self.generate_directions_str(results)
+
+    def bare_directions(self, args, mode='driving'):
+        if len(args) < 2:
+            return 'Please supply an origin and a destination separated by a "->". like : Paris, France -> Marseille, France '
+        return self.get_directions(args[0], args[1], mode)
+
+
+    @botcmd(split_args_with='->')
+    def directions(self, mess, args):
+        """
+        Returns the driving directions from origin to destination using Google directions api.
+        !directions origin -> destination
+        """
+        return self.bare_directions(args)
+
+
+
