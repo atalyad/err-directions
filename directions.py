1cb915462bc88403a97bdaa5e9a5de19a34a797d
Guillaume BINET <gbin@gootz.net>
2012-08-17 22:35:52 +0200

    backward and forward compatibility boiler plate

--- directions.py
+++ directions.py
@@ -1,7 +1,15 @@
 import logging
 import re
-from errbot.botplugin import BotPlugin
-from errbot.jabberbot import botcmd
+
+# Backward compatibility
+from errbot.version import VERSION
+from errbot.utils import version2array
+if version2array(VERSION) >= [1,6,0]:
+    from errbot import botcmd, BotPlugin
+else:
+    from errbot.botplugin import BotPlugin
+    from errbot.jabberbot import botcmd
+
 from urllib import quote
 import simplejson
 from urllib2 import urlopen
